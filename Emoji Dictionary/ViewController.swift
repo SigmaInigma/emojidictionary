64dc8173203a65c0e774c8ff40e3d27a49f16209
Dakota Brown <brown.dakotachase@gmail.com>
2016-10-23 20:45:40 -0400

    Added a segue between the two view controllers. Also embedded a navigational controller so that you can get back to the first view after clicking an emoji.

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -30,6 +30,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "firstSegue", sender: "🌈")
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
